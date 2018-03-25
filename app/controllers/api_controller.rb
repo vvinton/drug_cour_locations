8c71517a77101c3875ba30807000617daf4da05f
William Flanagan <wflanagan@audienti.com>
2018-03-25 13:29:06 -0400

    Add api controller

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,7 @@
+class ApiController < ApplicationController
+  def states_by_type
+  end
+
+  def states_by_color
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   resources :programs_information
   resources :imports
+  namespace :api do
+    namespace :v1 do
+      get :states_by_type, to: 'api#states_by_type'
+      get :states_by_color, to: 'api#states_by_color'
+    end
+  end
   get :nearbys, to: 'programs_information#nearbys'
   get :statistic, to: 'programs_information#statistic'
   get :state_coordinators, to: 'state_coordinators#index'
