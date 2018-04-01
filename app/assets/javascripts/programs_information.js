45fd7aa862f116c250d70b67d53d89129a2b041b
devumar <landoffighter@gmail.com>
2018-04-01 19:55:37 +0500

    some minor fixes

--- app/assets/javascripts/programs_information.js
+++ app/assets/javascripts/programs_information.js
@@ -1,3 +1,16 @@
+// convert unorderlist in two colm by adding new class
 $('.stateUl').filter(function(){
     return this.childNodes.length > 10
 }).addClass('twoColumns');
+
+// change value of format to cvs
+$("#download").click(function(){
+    alert("The paragraph was clicked.");
+    $('#format').val("csv");
+});
+
+// change value of format 
+$("#search").click(function(){
+    alert("The paragraph was clicked.");
+    $('#format').val("");
+});
