46ceeebbe18e927bd6584a2c11091511bef70b92
William Flanagan <wflanagan@audienti.com>
2018-04-25 17:31:00 +0000

    Update

--- app/models/search_item.rb
+++ app/models/search_item.rb
@@ -2,8 +2,6 @@ class SearchItem < ApplicationRecord
   has_many :search_item_locations
   has_many :program_informations, through: :search_item_locations
 
-  searchkick
-
   def search_data
     {
       lat: lat,
