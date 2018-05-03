2c6ace77e74321083379cd0d9768815b291f3bcb
William Flanagan <wflanagan@audienti.com>
2018-05-03 15:38:38 +0000

    Update

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -1,25 +1,27 @@
 class ImportsController < ApplicationController
 
   def create
-    @import_file = Import.new
-    @import_file.mdb_content_type = params[:import][:content_type]
-    @import_file.mdb_file_name = params[:import][:file].original_filename
-    if @import_file.save!
-      @import_file.file.attach(
-        io: File.open(params[:import][:file].tempfile.path),
-        content_type: params[:import][:content_type],
-        filename: params[:import][:file].original_filename
-      )
-      SetupImportJob.perform_later(@import_file.id)
-      flash[:notice] = "Successfuly uploaded. Import will begin momentarily."
-    else
-      flash[:error]  = "There was a problem saving the import file. Please contact support."
+    begin
+      @import_file = Import.new(import_params)
+      @import_file.mdb_content_type = params[:import][:content_type]
+      @import_file.mdb_file_name = params[:import][:file].original_filename
+      if @import_file.save
+        @import_file.file.attach(
+          io: File.open(params[:import][:file].tempfile.path),
+          content_type: params[:import][:content_type],
+          filename: params[:import][:file].original_filename
+        )
+        SetupImportJob.perform_later(@import_file.id)
+        flash[:notice] = "Successfuly uploaded. Import will begin momentarily."
+      else
+        flash[:error]  = "There was a problem saving the import file. Please contact support."
+      end
+    rescue => e
+      puts "#{e.inspect}"
+      puts "#{e.backtrace}"
+      flash[:error]  = "There was an exception saving the import file. Please contact support."
     end
     redirect_to imports_path
-  rescue => e
-    puts "#{e.inspect}"
-    puts "#{e.backtrace}"
-    raise e
   end
 
   private
