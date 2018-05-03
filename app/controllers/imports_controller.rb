8bb65eed6a8f031e4b41c24750388cb63bfb4968
William Flanagan <wflanagan@audienti.com>
2018-05-03 15:47:53 +0000

    UPdatd

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -6,6 +6,8 @@ class ImportsController < ApplicationController
       @import_file.mdb_content_type = params[:import][:content_type]
       @import_file.mdb_file_name    = params[:import][:file].original_filename
       if @import_file.save
+        puts "Made it here"
+        puts "#{@import_file.file.attached?}"
         @import_file.file.attach(
           io: File.open(params[:import][:file].tempfile.path),
           content_type: params[:import][:content_type],
@@ -14,12 +16,12 @@ class ImportsController < ApplicationController
         SetupImportJob.perform_later(@import_file.id)
         flash[:notice] = "Successfuly uploaded. Import will begin momentarily."
       else
-        flash[:danger]  = "There was a problem saving the import file. Please contact support."
+        flash[:error]  = "There was a problem saving the import file. Please contact support."
       end
     rescue => e
       puts "#{e.inspect}"
       puts "#{e.backtrace}"
-      flash[:danger]  = "There was an exception storing the import file. Please contact support."
+      flash[:error]  = "There was an exception storing the import file. Please contact support."
     end
     redirect_to imports_path
   end
