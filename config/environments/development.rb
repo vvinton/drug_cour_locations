b77d3b30a3ccde3ec730c8d45fc5756b251fc6e8
Igor_Khlebnikoff <Igor.Khlebnikoff@gmail.com>
2018-05-04 03:09:35 +0300

    story: 2, return version to 2.4.3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  # storage service local
+  # storage service amazon
   config.active_storage.service = :amazon
 
   # Enable/disable caching. By default caching is disabled.
